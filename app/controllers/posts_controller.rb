4673fb399c1bfc5b3d1256fdd66b99b3c9876565
Giovanni Carvelli <giovannircarvelli@gmail.com>
2015-04-21 21:00:58 -0400

    Votes are now only accepted from IPs that are signed in
    Any user can still vote unlimited times; that is next.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -34,24 +34,26 @@ class PostsController < ApplicationController
     end
 
     def upvote
-        logger.info "current upvotes: #{Post.find(params[:id]).upvotes}"
-        post = Post.find(params[:id])
-        logger.info "upvoting..."
-        post.upvotes += 1
-        logger.info "new upvotes: #{post.upvotes}"
-        post.save
-        logger.info "saving..."
-        redirect_to post
+        if signed_in?
+            logger.info "current upvotes: #{Post.find(params[:id]).upvotes}"
+            post = Post.find(params[:id])
+            logger.info "upvoting..."
+            post.upvotes += 1
+            logger.info "new upvotes: #{post.upvotes}"
+            post.save
+            logger.info "saving..."
+        end
     end
 
     def downvote
-        logger.info "current upvotes: #{Post.find(params[:id]).upvotes}"
-        post = Post.find(params[:id])
-        logger.info "upvoting..."
-        post.downvotes += 1
-        logger.info "new upvotes: #{post.upvotes}"
-        post.save
-        logger.info "saving..."
-        redirect_to post
+        if signed_in?
+            logger.info "current upvotes: #{Post.find(params[:id]).upvotes}"
+            post = Post.find(params[:id])
+            logger.info "upvoting..."
+            post.downvotes += 1
+            logger.info "new upvotes: #{post.upvotes}"
+            post.save
+            logger.info "saving..."
+        end
     end
 end
